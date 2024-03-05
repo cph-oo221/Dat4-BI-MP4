bde327eae6a4270d1a0945843684ebd7c4631057
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2024-03-05 13:36:10 +0100

    Streamlit app

---
+++ streamlit/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(
+    page_title="MP5",
+    page_icon="🧊",
+    layout="wide",
+)
+
+df = pd.read_csv("../data/HR-Employee-Attrition.csv")
+
+st.markdown(
+    """
+    ### Finding user input related to text pieces
+    <style>
+        .stButton>button:focus:not(:active) {
+            border: none !important;
+        }
+        .stButton>button:hover {
+            border-color: green !important;
+        }
+        .stButton>button {
+            color : white !important;
+        }
+    </style>
+    """
+, unsafe_allow_html=True)
+
+
+user_input = st.text_input("User input", "Type Here ...")
+
+
+if st.button("Submit"):
+    st.write(user_input)
